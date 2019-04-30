0308bccad53d8a118fe738df2e1002aea2de5a5e
daquinohd <daquinohd@noreploy.github.com>
2019-04-30 14:53:27 -0400

    Removed legacy NCIAnalyticsFunctions.js

--- docroot/profiles/custom/cgov_site/themes/custom/cgov/src/libraries/analytics/nci-analytics-functions.js
+++ docroot/profiles/custom/cgov_site/themes/custom/cgov/src/libraries/analytics/nci-analytics-functions.js
@@ -1867,9 +1867,32 @@ var NCIAnalytics = {
 
         clickParams.LogToOmniture();
     },
+
+    /* ********************************************************************** */
+    // TableSortHeaderClick: function(sender, pageName) {
+    //     clickParams = new NCIAnalytics.ClickParams(sender,
+    //         'nciglobal', 'o', 'SortTableHeaderClick');
+
+    //     clickParams.Props = {
+    //         05: "table_sort",
+    //         67: pageName
+    //     }
+    //     clickParams.LogToOmniture(); 
+    // },
+    /* ********************************************************************** */
+    ProfilePanelLinkClick: function(sender, linkText, pageName) {
+        clickParams = new NCIAnalytics.ClickParams (sender,
+            'nciglobal', 'o', 'ProfilePanelLinkClick'); 
+        
+        clickParams.Props = {
+            66: "InstitutionCard_" + pageName + "_" + linkText,
+            67: 'D=pageName'
+        }
+        clickParams.LogToOmniture();
+    } 
 };
 
-
+/* End the giant NCIAnalytics object functions */
 /* ********************************************************************** */
 /* ********************************************************************** */
 /* ********************************************************************** */
