940ecff7f021797414de05121ebc59399d0fe79e
daquinohd <daquinohd@noreploy.github.com>
2019-05-21 12:54:37 -0400

    Fixed PublicUse block

--- docroot/profiles/custom/cgov_site/modules/custom/cgov_core/src/Plugin/Block/PublicUse.php
+++ docroot/profiles/custom/cgov_site/modules/custom/cgov_core/src/Plugin/Block/PublicUse.php
@@ -3,7 +3,9 @@
 namespace Drupal\cgov_core\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -18,6 +20,35 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class PublicUse extends BlockBase implements ContainerFactoryPluginInterface {
 
   /**
+   * The route matcher.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatcher;
+
+  /**
+   * Constructs a CgovDisqus object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_matcher
+   *   The route matcher.
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    RouteMatchInterface $route_matcher
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->routeMatcher = $route_matcher;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
@@ -25,22 +56,48 @@ class PublicUse extends BlockBase implements ContainerFactoryPluginInterface {
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('cgov_core.cgov_navigation_manager')
+      $container->get('current_route_match')
     );
+  }
+
+  /**
+   * Gets the current entity if there is one.
+   *
+   * @return Drupal\Core\Entity\ContentEntityInterface
+   *   The retrieved entity, or FALSE if none found.
+   */
+  private function getCurrEntity() {
+    $params = $this->routeMatcher->getParameters()->all();
+    foreach ($params as $param) {
+      if ($param instanceof ContentEntityInterface) {
+        // If you find a ContentEntityInterface stop iterating and return it.
+        return $param;
+      }
+    }
+    return FALSE;
   }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $page_title = 'DEBUG page title';
-
     $build = [
       '#theme' => 'cgov_common',
       '#type' => 'block',
-      'page_title' => $page_title,
+      'page_title' => $this->getPublicPageTitle(),
     ];
     return $build;
+  }
+
+  /**
+   * Retrieve the title from the curent node.
+   */
+  private function getPublicPageTitle() {
+    // Get entity object.
+    if ($current = $this->getCurrEntity()) {
+      $page_title = $current->title->value;
+    }
+    return isset($page_title) ?? '';
   }
 
 }
